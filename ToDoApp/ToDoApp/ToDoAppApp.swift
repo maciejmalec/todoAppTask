53a30b71ead35f713d340ddfab604b496956c023
maciejmalec <malecmaciej.mail@gmail.com>
2022-05-25 12:33:27 +0200

    empty xcode project add

---
+++ ToDoApp/ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Maciej Malec on 25/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
